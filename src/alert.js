edb8b2dc3a360894aa66fceebdf1e14e8a6108f2
Brandon <brandon.lopez@puebla.gob.mx>
2019-02-05 12:52:44 -0600

    Trying alert component with some package. Not working

---
+++ src/alert.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Provider as AlertProvider } from 'react-alert'
+//import AlertTemplate from  'react-alert-template-basic';
+
+  class MAlert extends React.Component{
+      render(){
+        return(
+            alert('Hi')
+        );
+      }
+  }
+
+  export default MAlert;
